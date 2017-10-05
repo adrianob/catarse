b816ddcb58979368bc833e4cce99f10b550aa039
Adriano Benin <adriano.benin@gmail.com>
2017-10-05 17:06:46 -0300

    add goal validations

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -5,6 +5,7 @@ class Goal < ActiveRecord::Base
   include I18n::Alchemy
   belongs_to :project
 
-  validates_numericality_of :value, greater_than: 9, allow_blank: true
+  validates_numericality_of :value, greater_than: 9, allow_blank: false
+  validates_presence_of :description
 
 end
